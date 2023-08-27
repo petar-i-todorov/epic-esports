02ae9a835333db412cfe9ab9ae917da735e9ff2f
Petar Ivanov <46651r@unibit.bg>
2023-08-28 00:46:04 +0300

    fix esm  modules conflict

--- remix.config.js
+++ remix.config.js
@@ -9,6 +9,7 @@ module.exports = {
 	// serverBuildPath: "build/index.js",
 	// publicPath: "/build/",
 	// serverModuleFormat: 'esm',
+	serverDependenciesToBundle: ['chalk', '#ansi-styles', '#supports-color'],
 	future: {
 		v2_dev: true,
 		v2_errorBoundary: true,
