8ef960934890f23361b82efea75fd9533729cd1d
Petar Ivanov <46651r@unibit.bg>
2023-08-31 04:05:59 +0300

    add some styling & render featured posts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import { json, type V2_MetaFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
-import { formatDistanceToNow } from 'date-fns'
+import { formatDistanceToNow, subMonths } from 'date-fns'
 import { prisma } from '~/utils/prisma-client.server'
 
 export const meta: V2_MetaFunction = () => {
@@ -43,30 +43,76 @@ export const loader = async () => {
 		take: 10,
 	})
 
-	return json({ posts })
+	const oneMonthAgo = subMonths(new Date(), 1).toISOString()
+
+	const featuredPosts = await prisma.$queryRawUnsafe(
+		"select * from post where createdAt <= '$1';",
+		oneMonthAgo,
+	)
+
+	return json({ posts, featuredPosts })
 }
 
 export default function Index() {
-	const { posts } = useLoaderData<typeof loader>()
+	const { posts, featuredPosts } = useLoaderData<typeof loader>()
+
+	console.log(featuredPosts)
 
 	if (Array.isArray(posts) && posts.length > 0) {
 		return (
-			<div className="w-4/6 mx-auto mt-[80px]">
+			<div className="w-4/6 mx-auto mt-[80px] flex gap-[25px]">
 				<div className="w-[760px]">
-					<img
-						className="w-[100%] h-[425px]"
-						src={`resources/image/${posts[0].images[0].id}`}
-						alt={posts[0].images[0].id}
-					/>
-					<div className="p-5 bg-black">
-						<div className="mb-1 flex justify-between">
-							<span className="text-yellow-300">{posts[0].category.name}</span>
-							<span className="text-yellow-300">{`${formatDistanceToNow(
-								new Date(posts[0].createdAt),
-							).toUpperCase()} AGO`}</span>
+					<div className="mb-[30px]">
+						<img
+							className="w-[100%] h-[425px]"
+							src={`resources/image/${posts[0].images[0].id}`}
+							alt={posts[0].images[0].id}
+						/>
+						<div className="p-5 bg-black">
+							<div className="mb-1 flex justify-between">
+								<span className="text-yellow-300">
+									{posts[0].category.name}
+								</span>
+								<span className="text-yellow-300">{`${formatDistanceToNow(
+									new Date(posts[0].createdAt),
+								).toUpperCase()} AGO`}</span>
+							</div>
+							<span className="text-3xl text-white">{posts[0].title}</span>
 						</div>
-						<span className="text-3xl text-white">{posts[0].title}</span>
 					</div>
+					{posts.slice(1).map((post, index) => (
+						<>
+							<div
+								// eslint-disable-next-line no-negated-condition
+								className={`flex gap-[20px] ${index !== 0 ? 'mt-[20px]' : ''} ${
+									// eslint-disable-next-line no-negated-condition
+									index !== posts.length - 1 ? 'mb-[20px]' : ''
+								}`}
+								key={post.id}
+							>
+								<img
+									className="w-[250px] h-[141px]"
+									src={`resources/image/${post.images[0].id}`}
+									alt={post.images[0].altText ?? ''}
+								/>
+								<div className="w-[100%] flex flex-col gap-[10px]">
+									<span className="flex justify-between">
+										<span>{post.category.name}</span>
+										<span>{`${formatDistanceToNow(
+											new Date(posts[0].createdAt),
+										).toUpperCase()} AGO`}</span>
+									</span>
+									<h2 className="font-bold text-lg">{post.title}</h2>
+									<h3>{post.subtitle}</h3>
+								</div>
+							</div>
+							{index === posts.length - 2 || <hr />}
+						</>
+					))}
+				</div>
+				<div className="flex-grow">
+					FEATURED STORIES
+					<hr className="my-[20px]" />
 				</div>
 			</div>
 		)
