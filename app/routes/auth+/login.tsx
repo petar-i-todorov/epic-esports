ad6196d76689a92e00192f71e1e067e6d730d258
Petar Ivanov <46651r@unibit.bg>
2023-09-12 22:03:36 +0300

    signup ui

--- app/routes/auth+/login.tsx
+++ app/routes/auth+/login.tsx
@@ -1,69 +1,93 @@
-import { Link } from '@remix-run/react'
+import { Form, Link } from '@remix-run/react'
 import facebookLogoSrc from '#app/assets/auth-logos/facebook-logo.jpg'
 import appleLogoSrc from '#app/assets/auth-logos/apple-logo.jpg'
 import googleLogoSrc from '#app/assets/auth-logos/google-logo.jpg'
 
+const ServiceLogo = ({ src, alt }: { src: string; alt: string }) => (
+	<img
+		className="object-cover object-center"
+		src={src}
+		alt={alt}
+		width="45"
+		height="45"
+	/>
+)
+
+export const AuthButton = ({ children }: { children: string }) => (
+	<button className="h-[36px] self-stretch text-black bg-yellow-300 font-bold rounded-sm hover:text-white hover:bg-blue-600">
+		{children}
+	</button>
+)
+
+export const authInputsClassNames =
+	'h-[36px] p-2 self-stretch border-2 border-black text-black placeholder:text-gray-400'
+
+export const AuthPage = ({
+	action,
+	children,
+}: {
+	action?: string
+	children: React.ReactNode
+}) => (
+	<div className="flex-grow grid place-content-center dark:text-white">
+		<div className="w-[400px] p-[30px] border-2 border-black dark:border-white rounded-lg text-sm">
+			<div className="w-fit mx-auto mb-[10px] text-5xl">#</div>
+			<Form className="flex flex-col gap-2 items-center" action={action}>
+				{children}
+			</Form>
+		</div>
+	</div>
+)
+
 export default function LoginRoute() {
-	const loginInputsClassNames =
-		'h-[36px] p-2 self-stretch border-2 border-black text-black placeholder:text-gray-400'
 	return (
-		<div className="flex-grow grid place-content-center dark:text-white">
-			<div className="w-[400px] h-[500px] p-[30px] flex flex-col gap-2 items-center border-2 border-black dark:border-white rounded-lg text-sm">
-				<span className="text-5xl">#</span>
-				<div className="w-fit my-auto flex flex-col gap-2">
-					<span>Sign in with your social account</span>
-					<div className="flex justify-between">
-						<img
-							src={facebookLogoSrc}
-							width="45"
-							height="45"
-							alt="Facebook Logo"
-						/>
-						<img src={googleLogoSrc} alt="Google Logo" width="45" height="45" />
-						<img src={appleLogoSrc} alt="Apple Logo" width="45" height="45" />
-					</div>
-				</div>
-				<span className="font-bold">Sign in with your email</span>
-				<label className="sr-only" htmlFor="login-email">
-					Enter your email:
-				</label>
-				<input
-					className={loginInputsClassNames}
-					type="text"
-					placeholder="janedoe@email.com"
-					id="login-email"
-				/>
-				<label className="sr-only" htmlFor="login-password">
-					Enter your password:
-				</label>
-				<input
-					className={loginInputsClassNames}
-					type="text"
-					placeholder="janedoe123"
-					id="login-password"
-				/>
-				<label className="self-start">
-					<input type="checkbox" /> Keep me signed in
-				</label>
-				<Link
-					className="self-end text-blue-600 dark:text-blue-300 hover:underline"
-					to="#"
-				>
-					Forgot your password?
-				</Link>
-				<button className="h-[36px] self-stretch text-black bg-yellow-300 font-bold rounded-sm hover:text-white hover:bg-blue-600">
-					Sign in
-				</button>
-				<div className="flex gap-2">
-					<span>Don&apos;t have an account?</span>
-					<Link
-						className="text-blue-600 dark:text-blue-300 hover:underline"
-						to="/auth/signup"
-					>
-						Sign up now
-					</Link>
+		<AuthPage>
+			<div className="w-fit my-[20px] flex flex-col gap-2">
+				<span>Sign in with your social account</span>
+				<div className="flex justify-between">
+					<ServiceLogo src={facebookLogoSrc} alt="Facebook Logo" />
+					<ServiceLogo src={googleLogoSrc} alt="Google Logo" />
+					<ServiceLogo src={appleLogoSrc} alt="Apple Logo" />
 				</div>
 			</div>
-		</div>
+			<span className="font-bold">Sign in with your email</span>
+			<label className="sr-only" htmlFor="login-email">
+				Enter your email:
+			</label>
+			<input
+				className={authInputsClassNames}
+				type="text"
+				placeholder="janedoe@email.com"
+				id="login-email"
+			/>
+			<label className="sr-only" htmlFor="login-password">
+				Enter your password:
+			</label>
+			<input
+				className={authInputsClassNames}
+				type="text"
+				placeholder="janedoe123"
+				id="login-password"
+			/>
+			<label className="self-start">
+				<input type="checkbox" /> Keep me signed in
+			</label>
+			<Link
+				className="self-end text-blue-600 dark:text-blue-300 hover:underline"
+				to="#"
+			>
+				Forgot your password?
+			</Link>
+			<AuthButton>Sign in</AuthButton>
+			<div className="flex gap-2">
+				<span>Don&apos;t have an account?</span>
+				<Link
+					className="text-blue-600 dark:text-blue-300 hover:underline"
+					to="/auth/signup"
+				>
+					Sign up now
+				</Link>
+			</div>
+		</AuthPage>
 	)
 }
--- app/routes/auth+/signup.tsx
+++ app/routes/auth+/signup.tsx
@@ -1,3 +1,32 @@
+import {
+	authInputsClassNames,
+	AuthButton,
+	AuthPage,
+} from '#app/routes/auth+/login'
+import Link from '~/components/ui/custom-link'
+
 export default function SignupRoute() {
-	return <h1>SIGNUP</h1>
+	return (
+		<AuthPage>
+			<input className={authInputsClassNames} type="email" name="email" />
+			<input className={authInputsClassNames} type="password" name="password" />
+			<input
+				className={authInputsClassNames}
+				type="password"
+				name="confirm-password"
+			/>
+			<input className={authInputsClassNames} type="text" name="username" />
+			<input className={authInputsClassNames} type="text" name="fullname" />
+			<label>
+				<input type="checkbox" name="terms" /> By creating an account, I agree
+				with EPIC Esports <Link to="/privacy">Privacy Policy</Link> and{' '}
+				<Link to="/terms-and-conditions">Terms of Use</Link>.
+			</label>
+			<label>
+				<input type="checkbox" name="promotions" /> I would like to receive
+				updates and promotions from EPIC Esports.
+			</label>
+			<AuthButton>Accept & Create Account</AuthButton>
+		</AuthPage>
+	)
 }
