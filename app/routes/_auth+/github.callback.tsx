c10f7448558208463724779f5c58d3e9ec895a95
Petar Ivanov <46651r@unibit.bg>
2023-11-26 01:31:46 +0200

    feat: auth w/ github

--- app/routes/_auth+/github.callback.tsx
+++ app/routes/_auth+/github.callback.tsx
@@ -1,6 +1,8 @@
 import { DataFunctionArgs, redirect } from '@remix-run/node'
 import { authenticator } from '~/utils/auth-github.server'
+import { prisma } from '~/utils/prisma-client.server'
 import { createCookie } from '~/utils/verify.server'
+import { createCookie as createSessionCookie } from '#app/utils/session.server'
 
 export async function loader({ request }: DataFunctionArgs) {
 	const profile = await authenticator.authenticate('github', request, {
@@ -14,6 +16,52 @@ export async function loader({ request }: DataFunctionArgs) {
 		fullName: profile.fullName,
 		username: profile.username,
 	})
+
+	const connection = await prisma.connection.findUnique({
+		select: {
+			userId: true,
+		},
+		where: {
+			providerId_provider: {
+				provider: 'github',
+				providerId: profile.id,
+			},
+		},
+	})
+
+	if (connection) {
+		return redirect('/', {
+			headers: {
+				'Set-Cookie': await createSessionCookie(connection.userId),
+			},
+		})
+	}
+
+	const user = await prisma.user.findUnique({
+		select: {
+			id: true,
+		},
+		where: {
+			email: profile.email,
+		},
+	})
+
+	if (user) {
+		await prisma.connection.create({
+			data: {
+				id: profile.id,
+				provider: 'github',
+				providerId: profile.id,
+				userId: user.id,
+			},
+		})
+
+		return redirect('/', {
+			headers: {
+				'Set-Cookie': await createSessionCookie(user.id),
+			},
+		})
+	}
 
 	return redirect('/onboarding', {
 		headers: {
