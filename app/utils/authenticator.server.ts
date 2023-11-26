e25af53eb882d1654459faf3898c9ca39ffa67b7
Petar Ivanov <46651r@unibit.bg>
2023-11-26 03:35:40 +0200

    oauth for facebook

--- app/utils/authenticator.server.ts
+++ app/utils/authenticator.server.ts
@@ -3,6 +3,7 @@ import { createCookieSessionStorage } from '@remix-run/node'
 import { Authenticator } from 'remix-auth'
 import { GitHubStrategy } from 'remix-auth-github'
 import { GoogleStrategy } from 'remix-auth-google'
+import { FacebookStrategy } from 'remix-auth-socials'
 
 export type ProviderData = {
 	id: string
@@ -35,7 +36,6 @@ authenticator.use(
 			callbackURL: `${process.env.ORIGIN}/github/callback`,
 		},
 		async ({ profile }) => {
-			console.log(profile)
 			return {
 				username: profile.displayName,
 				fullName: profile.name.givenName,
@@ -66,6 +66,26 @@ authenticator.use(
 		},
 	),
 	'google',
+)
+
+authenticator.use(
+	new FacebookStrategy(
+		{
+			clientID: process.env.FACEBOOK_STRATEGY_CLIENT_ID!,
+			clientSecret: process.env.FACEBOOK_STRATEGY_CLIENT_SECRET!,
+			callbackURL: `${process.env.ORIGIN}/facebook/callback`,
+		},
+		async ({ profile }) => {
+			return {
+				username: profile.displayName,
+				fullName: profile.name.givenName,
+				email: profile.emails[0].value,
+				id: profile.id,
+				provider: profile.provider,
+			}
+		},
+	),
+	'facebook',
 )
 
 export { authenticator }
